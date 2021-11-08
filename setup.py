d2e702b7e749427ecfa7137246c5e090e962dac5
redhoneybee <flack319@gmail.com>
2021-11-08 22:40:18 +0900

    first init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "cointools",
+    version = "1.0.0",
+    description = "simple using threading to invest about bitcoin.",
+    author = "redhoneybee",
+    author_email= "flack319@gmail.com",
+    url = "https://github.com/Redhoneybee/cointools",
+    download_url="https://github.com/Redhoneybee/cointools/archive/master.zip",
+    keywords=["bitcoin", "invest", "thread", "threading", "tools"],
+    python_requires     = '>=3',
+)
